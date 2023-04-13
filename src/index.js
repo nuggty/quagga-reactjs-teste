97746baaf56179b6a28f359da311e18e529cf71c
Robert Pereira <robert.pdj@outlook.com>
2023-04-13 08:15:19 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import Scanner from "./components/Scanner";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  const [camera, setCamera] = useState(false);
+  const [result, setResult] = useState(null);
+
+  const onDetected = result => {
+    setResult(result);
+  };
+
+  return (
+    <div className="App">
+      <p>{result ? result : "Scanning..."}</p>
+      <button onClick={() => setCamera(!camera)}>
+        {camera ? "Stop" : "Start"}
+      </button>
+      <div className="container">
+        {camera && <Scanner onDetected={onDetected} />}
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
